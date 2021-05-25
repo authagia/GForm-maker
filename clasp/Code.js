0d69e0e50fae2eae9f712885fe55cb21101d4256
authagia <tkd2246@kkmail.be>
2021-05-26 00:44:43 +0900

    add function readTest in Code.js add bot command readTest in slb.py

--- clasp/Code.js
+++ clasp/Code.js
@@ -1,3 +1,16 @@
 function myFunction() {
-  return 'Hello World!'
+  const ss = SpreadsheetApp.getActive();
+  const sheet = ss.getSheets()[0];
+  const values = sheet.getDataRange().getValues();
+  return values  
+}
+
+function readTest(r,c){
+  if(!r&&!c){r=2;c=3}
+  console.log(r+','+c)
+  const ss = SpreadsheetApp.getActive();
+  const sheet = ss.getSheets()[0];
+  const value = sheet.getDataRange().getValues()[r][c];
+  console.log(value);
+  return value  
 }
--- slash_bot.py
+++ slash_bot.py
@@ -1,6 +1,8 @@
 import discord
 from discord.ext import commands
 from discord_slash import SlashCommand, SlashContext
+from discord_slash.utils.manage_commands import create_option
+import subprocess
 
 with open('./scrt/D.token') as f:
     token = f.read()
@@ -10,14 +12,41 @@ with open('./scrt/D.guild') as f:
 
 bot = commands.Bot(command_prefix='/' ,intents=discord.Intents.all())
 
-slash = SlashCommand(bot)
+slash = SlashCommand(bot,sync_commands=True)
 
-@slash.slash(guild_ids=guild_id, 
-                 name='Hello',
-                 description='This is just a test command, nothing more.',
-                )
-
+@slash.slash(guild_ids=guild_id,
+             name='Hello',
+             description='This is just a test command, nothing more.',
+            )
 async def test(ctx:SlashContext):
     await ctx.send(content="Hello World!")
+
+@slash.slash(guild_ids=guild_id,
+             name='readTest',
+             description='test for read data of specified posision in the spreadsheet.',
+             options=[
+	         create_option(
+                     name='row',
+                     description='set the row number you want to pick',
+                     option_type=4,
+                     required=True,
+                 ),
+                 create_option(
+		     name='column',
+		     description='set the column number you want to pick',
+		     option_type=4,
+                     required=True,
+                 ),
+             ],
+            )
+async def readTest(ctx,row , column):
+    await ctx.defer()
+    result = subprocess.check_output(
+                                     ['clasp','run','readTest','-p','['+str(row)+','+str(column)+']'],
+				     shell = False,
+				     cwd = './clasp/',
+                                    ).decode().split('1G')[1]
+    print(result)
+    await ctx.send(str(result))
 
 bot.run(token)
